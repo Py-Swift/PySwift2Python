aa5e96abdd25e7e049c524c4ff1909b3e60d344d
Py-Swift <psychowaspx@gmail.com>
2026-02-17 00:39:01 +0100

    Update PySwiftAST dependency to use version tag instead of branch

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ import PackageDescription
 
 let package_dependencies: [Package.Dependency] = [
     .package(url: "https://github.com/swiftlang/swift-syntax.git", from: "601.0.0"),
-    .package(url: "https://github.com/Py-Swift/PySwiftAST", branch: "master"),
+    .package(url: "https://github.com/Py-Swift/PySwiftAST", from: .init(0, 0, 0)),
     .package(url: "https://github.com/kylef/PathKit", .upToNextMajor(from: "1.0.1")),
 ]
 
